6b3aca018b9cf93304360dbf8befba8a78670ab2
Rohit Singhal <rohitsinghal1327@gmail.com>
2018-10-09 23:48:14 +0530

    check if .c files are available

---
+++ AppFeeds/TestC.c
@@ -0,0 +1,9 @@
+//
+//  TestC.c
+//  AppFeeds
+//
+//  Created by Rohit Singhal on 10/9/18.
+//  Copyright © 2018 Rohit Singhal. All rights reserved.
+//
+
+#include "TestC.h"
---
+++ AppFeeds/TestC.h
@@ -0,0 +1,14 @@
+//
+//  TestC.h
+//  AppFeeds
+//
+//  Created by Rohit Singhal on 10/9/18.
+//  Copyright © 2018 Rohit Singhal. All rights reserved.
+//
+
+#ifndef TestC_h
+#define TestC_h
+
+#include <stdio.h>
+
+#endif /* TestC_h */
